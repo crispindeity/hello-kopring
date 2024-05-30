afead079ee6b4ce94e295c9a56dc7fea7c09399f
crispin <shoy1415@gmail.com>
2024-05-30 21:21:30 +0900

    :sparkles: Team register validation
      - 팀 등록 검증 로직 추가

--- src/main/kotlin/org/crispin/hellokopring/mini/employee/repository/EmployeeRepository.kt
+++ src/main/kotlin/org/crispin/hellokopring/mini/employee/repository/EmployeeRepository.kt
@@ -5,4 +5,5 @@ import org.crispin.hellokopring.mini.employee.domain.Employee
 interface EmployeeRepository {
     fun save(employee: Employee): Employee
     fun findAll(): List<Employee>
+    fun findById(id: Long): Employee?
 }
--- src/main/kotlin/org/crispin/hellokopring/mini/team/service/TeamService.kt
+++ src/main/kotlin/org/crispin/hellokopring/mini/team/service/TeamService.kt
@@ -1,17 +1,30 @@
 package org.crispin.hellokopring.mini.team.service
 
+import org.crispin.hellokopring.mini.employee.domain.Employee
+import org.crispin.hellokopring.mini.employee.repository.EmployeeRepository
 import org.crispin.hellokopring.mini.team.domain.Team
 import org.crispin.hellokopring.mini.team.repository.TeamRepository
 
 class TeamService(
-    private val teamRepository: TeamRepository
+    private val teamRepository: TeamRepository,
+    private val employeeRepository: EmployeeRepository
 ) {
 
     fun register(team: Team): Team {
+        verifyManagerRegisterEligibility(team)
         return teamRepository.save(team)
     }
 
     fun retrieveAll(): List<Team> {
         return teamRepository.findAll()
+    }
+
+    private fun verifyManagerRegisterEligibility(team: Team) {
+        team.managerId?.run {
+            val employee: Employee? = employeeRepository.findById(this)
+            require(employee != null && employee.teamId == team.id) {
+                "등록되지 않은 직원 아이디 입니다. ${team.managerId}"
+            }
+        }
     }
 }
